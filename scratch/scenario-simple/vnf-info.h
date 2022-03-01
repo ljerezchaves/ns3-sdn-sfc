d9458faaf72bfc8685ab6788fdcd1ef977381c56
Luciano J Chaves <ljerezchaves@gmail.com>
2022-03-01 20:21:34 -0300

    GNU GPL fix

--- scratch/scenario-simple/vnf-info.h
+++ scratch/scenario-simple/vnf-info.h
@@ -1,7 +1,5 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015 University of Campinas (Unicamp)
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
@@ -14,8 +12,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Author: Luciano Jerez Chaves <luciano@lrc.ic.unicamp.br>
  */
 
 #ifndef VNF_INFO_H
